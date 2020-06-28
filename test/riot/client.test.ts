78ab266b23ac1847eecd290d95a99743c03f464b
Patrick O'Brien <patrickobrien@pob.dev>
2020-06-28 12:44:18 -0500

    add test, trigger build
    testing actions

--- test/riot/client.test.ts
+++ test/riot/client.test.ts
@@ -1,8 +1,8 @@
-import { RiotClient, riotTokenHeader } from "../../src/riot/apis/client.ts";
+import { RiotClient, riotTokenHeader, appRateLimitCountHeader, appRateLimitHeader } from "../../src/riot/apis/client.ts";
 import { assert } from "https://deno.land/std/testing/asserts.ts";
 import { config } from "../../src/riot/config.ts";
 
-Deno.test("getHeaders", async () => {
+Deno.test("appliesApiHeader", async () => {
 	let headers: Headers | undefined = undefined;
 	const ogFetch = globalThis.fetch;
 	globalThis.fetch = (input: string | Request | URL, init?: RequestInit) => {
@@ -10,10 +10,32 @@ Deno.test("getHeaders", async () => {
 		return Promise.resolve(new Response());
 	}
 
-	const response = await RiotClient.get("test");
+	const _response = await RiotClient.get("test");
 
 	assert(headers !== undefined);
-	assert(headers!.get(riotTokenHeader) === config.API_KEY);
+	assert(headers!.get(riotTokenHeader) === config.API_KEY, "Expected api key to be supplied to header.");
 
+	globalThis.fetch = ogFetch;
+});
+
+Deno.test("appliesRateLimiting", async () => {
+	let tries = 0;
+	const ogFetch = globalThis.fetch;
+	globalThis.fetch = (_input: string | Request | URL, _init?: RequestInit) => {
+		const headers = new Headers();
+		if (tries === 0) {
+			headers.set(appRateLimitHeader, "1,1:1,1");
+			headers.set(appRateLimitCountHeader, "1,1:1,1");
+		}
+		const response = new Response(null, {
+			headers
+		});
+		tries++;
+		return Promise.resolve(response);
+	}
+
+	const _response = await RiotClient.get("test");
+
+	assert(tries > 1, `Expected API call to retry blocked call. Tries: ${tries}`);
 	globalThis.fetch = ogFetch;
 });
